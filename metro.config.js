c50023d86f087b5d85dcd652d857e62a3901af42
BohdanSamusko <bohdan.samusko@gmail.com>
2024-07-24 17:50:09 +0300

    update for metro.config.js (2)

--- metro.config.js
+++ metro.config.js
@@ -1,25 +1,32 @@
-const path = require("path");
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
-const { resolver: { sourceExts, assetExts } } = defaultConfig;
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
+const {
+  resolver: { sourceExts, assetExts },
+} = getDefaultConfig(__dirname);
+
 const config = {
   transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
   resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"],
-    resolverMainFields: ["sbmodern", "react-native", "browser", "main"],
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
   },
-  watchFolders: [path.resolve(__dirname, "../")],
 };
 
 module.exports = mergeConfig(defaultConfig, config);
